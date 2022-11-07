dcb36cb9fbb8176b4ae3471678171085e422e059
Genie <frothsavd@gmail.com>
2022-11-07 12:49:51 -0500

    Save settings to localstorage

--- src/app/grid/grid-settings/grid-settings.service.ts
+++ src/app/grid/grid-settings/grid-settings.service.ts
@@ -8,9 +8,25 @@ import { DEFAULT_AXIS_OPTIONS, DEFAULT_PARAMS, GridForm } from '../types'
 export class GridSettingsService {
     reload$ = new Subject<any>()
 
-    gridForm: GridForm = {
-        baseParams: DEFAULT_PARAMS,
-        axisOptions: DEFAULT_AXIS_OPTIONS,
-        activeAxis: { x: 'size', y: 'seed' },
+    gridForm!: GridForm
+
+    constructor() {
+        const savedGrid = window.localStorage.getItem('gridForm')
+        if (savedGrid) {
+            this.gridForm = JSON.parse(savedGrid)
+        } else {
+            this.gridForm = {
+                baseParams: DEFAULT_PARAMS,
+                axisOptions: DEFAULT_AXIS_OPTIONS,
+                activeAxis: { x: 'size', y: 'seed' },
+            }
+        }
+
+        this.reload$.subscribe(() => {
+            window.localStorage.setItem(
+                'gridForm',
+                JSON.stringify(this.gridForm)
+            )
+        })
     }
 }
